1d7fcca9259956e921d4559f6cebc895411f5ac6
Tomek Barabasz <tomek.barabasz@gmail.com>
2022-04-22 10:52:56 +0200

    some more good stuff

--- asteroids-04.py
+++ asteroids-04.py
@@ -27,7 +27,9 @@ class Ship:
         self.angle = 0
         self.is_accel = False
         self.wrap = wrap
-
+        self.ToDraw = []
+        self.hitPoints = 0
+    
     def move_(self, events):
         self.pos,self.speed = self.wrap(self.pos, self.speed)
         rad = math.radians(self.angle+90)
@@ -50,8 +52,8 @@ class Ship:
         self.move_(events)
         Size = self.config.size
         Points = [(-Size,-Size),(0,2*Size),(Size,-Size)]
-        ToDraw = rotMove(self.angle, self.pos, Points)
-        filled_polygon(screen, ToDraw, self.config.color)
+        self.ToDraw = rotMove(self.angle, self.pos, Points)
+        filled_polygon(screen, self.ToDraw, self.config.color)
 
         if self.is_accel:
             Points = [(-Size,-Size),(Size,-Size),(0,-2*Size)]
@@ -74,6 +76,12 @@ class Ship:
         else:
             return []
     
+    def getBoundingPolygon(self):
+        return self.ToDraw
+    
+    def collideWith(self, asteroid, pt):
+        self.hitPoints += asteroid.size
+
 def got_quit_event(events):
     for event in events:
         if event.type == pygame.QUIT:
@@ -83,11 +91,14 @@ def got_quit_event(events):
                 return True
     return False
   
-def rotate(angle, Points):
+def makeRotMatrix(angle):
     rad = math.radians(angle)
     c = math.cos(rad)
     s = math.sin(rad)
-    ROT = np.array([c,-s, s, c]).reshape(2,2)
+    return np.array([c,-s, s, c]).reshape(2,2)
+
+def rotate(angleOrMatrix, Points):
+    ROT = makeRotMatrix(angle) if type(angleOrMatrix) in [int,float] else angleOrMatrix
     return ROT.dot(np.array(Points).T).T
 
 def move(dt,Points):
@@ -135,7 +146,7 @@ def makeBounce(width,height):
         return npos,speed
     return bounce
 
-def drawMissiles(Config,screen, Missiles, wrap):
+def drawMissiles(Config, screen, Missiles, wrap):
     tm = datetime.utcnow()
     ValidMissiles = []
     for m in Missiles:
@@ -145,8 +156,111 @@ def drawMissiles(Config,screen, Missiles, wrap):
             ValidMissiles.append( Missile(pos, speed, m.t0) )
     return ValidMissiles
 
-def drawAsteroids(screen, Asteroids, t0, wrap):
-    pass
+def drawAsteroids(Config, screen, Asteroids, tstart, wrap, screen_size):
+    for a in Asteroids:
+        a.pos,a.speed = wrap(a.pos,a.speed)
+        idx = a.size - 1
+        aacircle(screen, int(round(a.pos.x)), int(round(a.pos.y)), Config.size[idx], Config.color[idx])
+    totWeight = sum( [a.size for a in Asteroids])
+    delta_time = (datetime.utcnow() - tstart).total_seconds()
+    newWeight = delta_time * Config.weight_inc - totWeight
+    weight = Config.weights[-1]
+    if newWeight > weight:
+        width,height = screen_size
+        size = Config.size[-1]
+        angle = random.uniform(0,359)
+        speed = Vector2.fromPolar(Config.speed[-1], angle)
+        if angle < 90:
+            pos = Vector2(-size,height+size)
+        elif angle < 180:
+            pos = Vector2(width+size,height+size)
+        elif angle < 270:
+            pos = Vector2(width+size,-size)
+        else:
+            pos = Vector2(-size,-size)
+        Asteroids.append( Asteroid(pos,speed,weight) )
+    return Asteroids
+
+def asteroidHit(Config, asteroid,missile):
+    if asteroid.size == 1:
+        return []
+    elif asteroid.size == 2:
+        dir = missile.speed.unity()
+        pdir = Vector2(-dir.y,dir.x)
+        size = Config.asteroid.size[0]
+        speed = Config.asteroid.speed[0]
+        a1 = Asteroid(asteroid.pos + pdir*size, pdir*speed, 1)
+        a2 = Asteroid(asteroid.pos - pdir*size, pdir*-speed, 1)
+        return [a1,a2]
+    elif asteroid.size == 3:
+        size = Config.asteroid.size[1]
+        speed = Config.asteroid.speed[1]
+        dir = missile.speed.unity()
+        dist = 1.1547005383792515*size
+        a1 = Asteroid(asteroid.pos + dir*size, dir*speed, 2)
+        rot = makeRotMatrix(120)
+        dir = dir.dot(rot)
+        a2 = Asteroid(asteroid.pos + dir*size, dir*speed, 2)
+        dir = dir.dot(rot)
+        a3 = Asteroid(asteroid.pos + dir*size, dir*speed, 2)
+        return [a1,a2,a3]
+
+def collide(Config, Asteroids, Missiles, ship):
+    #ship vs Asteroids
+    if not Asteroids:
+        return Asteroids,Missiles
+    
+    ship_pts = ship.getBoundingPolygon()
+    Sizes = Config.asteroid.size
+    for a in Asteroids:
+        r = Sizes[a.size-1]
+        r_sq = r*r
+        for p in ship_pts:
+            d_sq = (p[0]-a.pos.x)**2 + (p[1]-a.pos.y)**2
+            if d_sq <= r_sq:
+                ship.collideWith(a,p)
+                break
+    
+    #Missiles vs Asteroids
+    if not Missiles:
+        return Asteroids,Missiles
+    
+    AliveMissiles = []
+    AliveAsteroids = []
+    for m in Missiles:
+        hit = False
+        AliveAsteroids = []
+        for a in Asteroids:
+            r = Sizes[a.size-1]
+            r_sq = r*r
+            d_sq = m.pos.distSq(a.pos)
+            if d_sq <= r_sq:
+                hit = True
+                AliveAsteroids += asteroidHit(Config,a,m)
+                break
+            else:
+                AliveAsteroids.append(a)
+
+        if not hit:
+            AliveMissiles.append(m)
+        Asteroids = AliveAsteroids
+
+    return AliveAsteroids,AliveMissiles
+
+def displayText(screen,font,color,background,Text,pos):
+    left,top = pos
+    for line in Text:
+        size = font.size(line)
+        ren = font.render(line, 0, color, background)
+        screen.blit(ren, (left,top))
+        top += int( round(size[1] * 1.2) )
+    return (left,top)
+
+def displayPoints(ship,screen,font,pos):
+    return displayText(screen,font,(255,255,255),(0,0,0),[f'hit {ship.hitPoints} times'],pos)
+
+def displayTimeStats(time_measurements,screen,font,pos):
+    return pos
 
 def main(Args):
     screen_size = width,height = tuple(map(int, Args.display.split(',')))
@@ -164,19 +278,29 @@ def main(Args):
     ship = Ship(Config.ship, width//2, height//2, wrap)
     Missiles = []
     Asteroids = []
-    t0 = datetime.utcnow()
+    tstart= datetime.utcnow()
+    font = pygame.font.SysFont("Arial", 20)
+    time_measurements = [0,0,0,0,0,0]
     while True:
-        t0 = datetime.utcnow()
+        time_measurements[0] = datetime.utcnow()
         events = pygame.event.get()
         if got_quit_event(events):
             break
         screen.fill(background)
+        time_measurements[1] = datetime.utcnow()
         ship.draw(screen,events)
         Missiles.extend(ship.fire(events))
+        time_measurements[2] = datetime.utcnow()
         Missiles = drawMissiles(Config.missile, screen, Missiles, bounce)
-        Asteroids = drawAsteroids(screen,Asteroids,t0,wrap)
+        time_measurements[3] = datetime.utcnow()
+        Asteroids = drawAsteroids(Config.asteroid, screen, Asteroids, tstart, wrap, screen_size)
+        time_measurements[4] = datetime.utcnow()
+        Asteroids,Missiles = collide(Config, Asteroids, Missiles, ship)
+        time_measurements[5] = datetime.utcnow()
+        tpos = displayPoints(ship,screen,font,(10,10))
+        displayTimeStats(time_measurements,screen,font,tpos)
         pygame.display.flip()
-        t1 = datetime.utcnow()
+        
     
     pygame.quit()
 
--- vector.py
+++ vector.py
@@ -1,12 +1,15 @@
+from math import sin,cos,pi,sqrt
+from numpy import dot
+
 class Vector2:
     def __init__(self,x,y):
         self.x = x
         self.y = y
     @staticmethod
     def fromPolar(length,angle):
-        deg = 2*math.pi*angle/180
-        x = length * math.sin(deg)
-        y = length * math.cos(deg)
+        deg = 2*pi*angle/180
+        x = length * sin(deg)
+        y = length * cos(deg)
         return Vector2(x,y)
     def __add__(self, other):
         return Vector2(self.x+other.x, self.y+other.y)
@@ -17,10 +20,18 @@ class Vector2:
     def lengthSq(self):
         return self.x*self.x + self.y*self.y
     def length(self):
-        return math.sqrt( self.lengthSq() )
+        return sqrt( self.lengthSq() )
+    def distSq(self,other):
+        dx = self.x-other.x
+        dy = self.y-other.y
+        return dx*dx+dy*dy
+    def dist(self,other):
+        return sqrt( self.distSq() )
     def unity(self):
         ool = 1.0/self.length()
-        return Vector2(x*ool,y*ool)
+        return Vector2(self.x*ool,self.y*ool)
     def __str__(self):
         return f'({self.x},{self.y})'
-        
+    def dot(self, matrix):
+        return Vector2(*dot(matrix,[self.x, self.y]))
+
